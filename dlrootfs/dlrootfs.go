d67b3c39b71802f0f4aee37c3a3e48fc78972639
Robin Monjo <robin.monjo@applidget.com>
2015-01-10 13:24:38 +0100

    use codegangsta/cli in main

--- dlrootfs/dlrootfs.go
+++ dlrootfs/dlrootfs.go
@@ -1,88 +1,83 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/codegangsta/cli"
 	"github.com/rmonjo/dlrootfs"
 )
 
 const VERSION string = "1.4.0"
 
-var (
-	pullFlagSet         = flag.NewFlagSet("pull", flag.ExitOnError)
-	rootfsDest  *string = pullFlagSet.String("d", "./rootfs", "destination of the resulting rootfs directory")
-	credentials *string = pullFlagSet.String("u", "", "docker hub credentials: <username>:<password>")
-	gitLayering *bool   = pullFlagSet.Bool("g", false, "use git layering")
-
-	pushFlagSet         = flag.NewFlagSet("push", flag.ExitOnError)
-	message     *string = pushFlagSet.String("m", "dlrootfs push", "commit message")
-	rootfs      *string = pushFlagSet.String("d", "./rootfs", "rootfs path")
-	creds       *string = pushFlagSet.String("u", "", "docker hub credentials: <username>:<password>")
-)
-
-func init() {
-	pullFlagSet.Usage = func() {
-		fmt.Fprintf(os.Stderr, "PULL:\n  dlrootfs pull <image_name>:[<image_tag>] [-d <rootfs_destination>] [-u <username>:<password>] [-g]\n\n")
-		fmt.Fprintf(os.Stderr, "DEFAULT:\n")
-		pullFlagSet.PrintDefaults()
-	}
-
-	pushFlagSet.Usage = func() {
-		fmt.Fprintf(os.Stderr, "PUSH:\n  dlrootfs push ...\n\n")
-		fmt.Fprintf(os.Stderr, "DEFAULT:\n")
-		pushFlagSet.PrintDefaults()
-	}
+var Commands = []cli.Command{
+	pullCmd,
+	pushCmd,
 }
 
-func globalUsage() {
-	fmt.Fprintf(os.Stderr, "GLOBAL USAGE:\n  dlrootfs pull\n  dlrootfs push\n\n")
-	pullFlagSet.Usage()
-	fmt.Fprintf(os.Stderr, "\n")
-	pushFlagSet.Usage()
+var pullCmd = cli.Command{
+	Name:        "pull",
+	Usage:       "pull <image_name>:[<image_tag>] [-g]",
+	Description: "pull an image from the dockerhub. Image name format is reposiroty/name:tag",
+	Action:      pull,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "g, git-layering",
+			Usage: "Download image in a git repository where each layer is a branch. Needed if you want to push your image afterward",
+		},
+	},
+}
+
+var pushCmd = cli.Command{
+	Name:        "push",
+	Usage:       "push <image_name>:[<image_tag>] [-m <comment>]",
+	Description: "push an image on the dockerhub. Image name format is repository/name:tag",
+	Action:      push,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "m, message",
+			Usage: "Message to indicates you changes",
+			Value: "dlrootfs push",
+		},
+	},
 }
 
 func main() {
 
-	if len(os.Args) <= 1 {
-		globalUsage()
-		return
+	app := cli.NewApp()
+	app.Name = "dlrootfs"
+	app.Version = VERSION
+	app.Usage = "docker hub without docker"
+	app.Author = "Robin Monjo"
+	app.Email = "robinmonjo@gmail.com"
+	app.Commands = Commands
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "u, user",
+			Usage: "dockerhub credentials with format <username>:<password>",
+		},
+		cli.StringFlag{
+			Name:  "r, rootfs",
+			Usage: "path where to store the rootfs (pull) or where to find the rootfs (push)",
+			Value: "./rootfs",
+		},
 	}
 
-	cmd := os.Args[1]
-	subArgs := os.Args[2:]
 	dlrootfs.PrintOutput = true
 
-	switch cmd {
-	case "pull":
-		pullCmd(subArgs)
-	case "push":
-		pushCmd(subArgs)
-	case "version":
-		versionCmd()
-	default:
-		globalUsage()
-	}
+	app.Run(os.Args)
 }
 
-func versionCmd() {
-	fmt.Println(VERSION)
-}
-
-func pullCmd(args []string) {
-	imageNameTag := args[0]
-
-	pullFlagSet.Parse(args[1:])
-
-	if imageNameTag == "" {
-		pullFlagSet.Usage()
+func pull(c *cli.Context) {
+	if len(c.Args()) == 0 {
+		cli.ShowSubcommandHelp(c)
 		return
 	}
 
-	imageName, imageTag := dlrootfs.ParseImageNameTag(imageNameTag)
-	userName, password := dlrootfs.ParseCredentials(*credentials)
+	imageName, imageTag := dlrootfs.ParseImageNameTag(c.Args()[0])
+	userName, password := dlrootfs.ParseCredentials(c.GlobalString("user"))
 
 	fmt.Printf("Opening a session for %v ...\n", imageName)
 	session, err := dlrootfs.NewHubSession(imageName, userName, password)
@@ -90,26 +85,22 @@ func pullCmd(args []string) {
 		log.Fatal(err)
 	}
 
-	err = session.DownloadFlattenedImage(imageName, imageTag, *rootfsDest, *gitLayering)
+	err = session.DownloadFlattenedImage(imageName, imageTag, c.GlobalString("rootfs"), c.Bool("git-layering"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("\nRootfs of %v:%v in %v\n", imageName, imageTag, *rootfsDest)
+	fmt.Printf("\nRootfs of %v:%v in %v\n", imageName, imageTag, c.GlobalString("rootfs"))
 }
 
-func pushCmd(args []string) {
-	imageNameTag := args[0]
-
-	pushFlagSet.Parse(args[1:])
-
-	if imageNameTag == "" {
-		pushFlagSet.Usage()
+func push(c *cli.Context) {
+	if len(c.Args()) == 0 {
+		cli.ShowSubcommandHelp(c)
 		return
 	}
 
-	imageName, imageTag := dlrootfs.ParseImageNameTag(imageNameTag)
-	userName, password := dlrootfs.ParseCredentials(*creds)
+	imageName, imageTag := dlrootfs.ParseImageNameTag(c.Args()[0])
+	userName, password := dlrootfs.ParseCredentials(c.GlobalString("user"))
 
 	fmt.Printf("Opening a session for %v ...\n", imageName)
 	session, err := dlrootfs.NewHubSession(imageName, userName, password)
@@ -118,12 +109,12 @@ func pushCmd(args []string) {
 	}
 
 	fmt.Printf("Extracting changes\n")
-	changes, err := dlrootfs.ExportChanges(*rootfs)
+	changes, err := dlrootfs.ExportChanges(c.GlobalString("rootfs"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = session.PushImageLayer(changes, imageName, imageTag, *message, *rootfs)
+	err = session.PushImageLayer(changes, imageName, imageTag, c.String("message"), c.GlobalString("rootfs"))
 	if err != nil {
 		log.Fatal(err)
 	}
